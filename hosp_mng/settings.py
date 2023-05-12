3077cd04b1af86bd904ff8c4920dbe550be7379c
nishaj8694 <nishajppvvm@gmail.com>
2023-05-12 20:11:40 +0530

    add

--- hosp_mng/settings.py
+++ hosp_mng/settings.py
@@ -50,10 +50,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap4',
     'razorpay',
-    'sweetify',
-    'mptt',
     'django_otp',
-    
 ]
 
 MIDDLEWARE = [
@@ -121,18 +118,27 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'samp',
+#         'USER': os.getenv("USER"),
+#         'PASSWORD': os.getenv("PASSWORD"),
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+        
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'samp',
-        'USER': os.getenv("USER"),
-        'PASSWORD': os.getenv("PASSWORD"),
-        'HOST': 'localhost',
+        'NAME': 'hospitalcourse',
+        'USER': 'postgres',
+        'PASSWORD': 'hospitalsinu123',
+        'HOST': 'hospitaldatabase.c6ohaempg5mj.eu-north-1.rds.amazonaws.com',
         'PORT': '5432',
-        
     }
 }
-
 AUTHENTICATION_BACKENDS = [    'home.backened.CustomAuthenticationBackend',]
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -186,3 +192,16 @@ RAZORPAY_SECRET_KEY=os.getenv('RAZORPAY_SECRET_KEY')
 
 ACCOUNT_SID=os.getenv('ACCOUNT_SID')
 AUTH_TOKEN=os.getenv('AUTH_TOKEN')
+
+
+
+
+AWS_ACCESS_KEY_ID = 'AKIASDSTEERB367H7RNY '
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'hospitalcourse'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'eu-north-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
